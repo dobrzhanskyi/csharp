e0f95475b99deff646ad116f3b9c7abbff726d65
Vladimir Dobrzhanskyi <33500265+dobrzhanskyi@users.noreply.github.com>
2018-04-15 19:51:16 +0300

    update

--- CashBox/CashBox/Operations.cs
+++ CashBox/CashBox/Operations.cs
@@ -7,27 +7,27 @@ namespace CashBox
 	//{
 	//	stock.Add(new Stock(getItemFromBarcode(barcode), count));
 	//}
+	//private Item GetItemFromBarcode(int barcode)
+	//{
+	//	foreach (var items in itemList)
+	//	{
+	//		if (items.Barcode == barcode)
+	//		{
+	//			return items;
+	//		}
+	//	}
+	//	return null;
+	//}
 
 	class Operations
 	{
 		public List<Item> itemList = new List<Item>();
+
 		public void createItem(int barcode, int price,int balance)
 		{
 			itemList.Add(new Item(barcode, price, balance));
 		}
 	
-		private Item GetItemFromBarcode(int barcode)
-		{
-			foreach (var items in itemList)
-			{
-				if (items.Barcode == barcode)
-				{
-					return items;
-				}
-			}
-			return null;
-		}
-
 		public void SellFromItem(int barcode,int count)
 		{		
 			for (int i = 0; i < itemList.Count; i++)
@@ -38,7 +38,7 @@ namespace CashBox
 					int tmpCount = item.Balance;
 					if (count >= tmpCount)
 					{
-						RemoveItemFromItem(item);
+						RemoveItem(item);
 						count -= tmpCount;
 						i--;
 					}
@@ -51,7 +51,7 @@ namespace CashBox
 			}
 		}
 
-		private void RemoveItemFromItem(Item item)
+		private void RemoveItem(Item item)
 		{
 			itemList.Remove(item);
 		}
