ff421f3efd9f86416a228caf76050388926bca1f
Vladimir Dobrzhanskyi <33500265+dobrzhanskyi@users.noreply.github.com>
2018-04-17 01:57:22 +0300

    few Addons
    -Menu
    -Keyboard
    -GeoLocation
    -Contacts receiving
    -api SmallTalk

--- TelegramBot/TelegramBot/Program.cs
+++ TelegramBot/TelegramBot/Program.cs
@@ -4,6 +4,9 @@ using Telegram.Bot.Args;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
 using Telegram.Bot.Types.InlineKeyboardButtons;
+using Telegram.Bot.Types;
+using ApiAiSDK;
+using ApiAiSDK.Model;
 
 
 namespace TelegramBot
@@ -11,9 +14,13 @@ namespace TelegramBot
 	class Program
 	{
 		static TelegramBotClient Bot;
+		static ApiAi apiAi;
 		static void Main(string[] args)
 		{
 			Bot = new TelegramBotClient("593986184:AAG--tKPoPzn81lELpe_zD-To0dVBloBvJ4");
+			AIConfiguration config = new AIConfiguration("65b5cc50b74a4d4b837b24e753a7a17d", SupportedLanguage.Russian);
+			apiAi = new ApiAi(config);
+			
 
 			Bot.OnMessage += Bot_OnMessageReceivedAsync;
 			Bot.OnCallbackQuery += BotOnCallBackQueryReceived;
@@ -25,9 +32,13 @@ namespace TelegramBot
 			Bot.StartReceiving();
 		}
 
-		private static void BotOnCallBackQueryReceived(object sender, Telegram.Bot.Args.CallbackQueryEventArgs e)
+		private static async void BotOnCallBackQueryReceived(object sender, Telegram.Bot.Args.CallbackQueryEventArgs e)
 		{
+			string buttonText = e.CallbackQuery.Data;
+			string name = $"{e.CallbackQuery.From.FirstName}{e.CallbackQuery.From.LastName}";
+			Console.WriteLine($"{name} нажал кнопку {buttonText}");
 
+			await Bot.AnswerCallbackQueryAsync(e.CallbackQuery.Id, $"Вы нажали кнопку {buttonText}");
 		}
 
 		private static async void Bot_OnMessageReceivedAsync(object sender, Telegram.Bot.Args.MessageEventArgs e)
@@ -56,7 +67,8 @@ namespace TelegramBot
 					{
 						new[]
 						{
-							InlineKeyboardButton.WithUrl("Web","http://pornhub.com")
+							InlineKeyboardButton.WithUrl("WebSearch","http://google.com"),
+							InlineKeyboardButton.WithUrl("Facebook","http://fb.com")
 						},
 						new[]
 						{
@@ -65,13 +77,34 @@ namespace TelegramBot
 						}
 
 					});
-					await Bot.SendTextMessageAsync(message.From.Id, "Выберите пункт", replyMarkup: inlineKeyboard);
+					await Bot.SendTextMessageAsync(message.From.Id, "Выберите пункт меню", replyMarkup: inlineKeyboard);
 					break;
 				case "/keyboard":
+					var replyKeyboard = new ReplyKeyboardMarkup(new[]
+			{
+					new[]
+					{
+						new KeyboardButton("Hello"),
+				  	new KeyboardButton("How are you?")
+					},
+					 new[]
+					{
+			    	new KeyboardButton("Contacts"){ RequestContact=true},
+				    new KeyboardButton("GeoLocation"){RequestLocation=true}
+					}
+			});
+					await Bot.SendTextMessageAsync(message.Chat.Id, "Message", replyMarkup: replyKeyboard);
 					break;
 				default:
+					var response = apiAi.TextRequest(message.Text);
+					string answer = response.Result.Fulfillment.Speech;
+					if (answer == "")
+						answer = "Прости, я тебя не понял";
+					await Bot.SendTextMessageAsync(message.From.Id, answer);
 					break;
 			}
 		}
 	}
 }
+
+
