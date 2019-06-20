5bb2fe1df00a75a60dbd9aa9368e337247eb3b0b
vdobrzhanskyi <vdobrzhanskyi@skelia.com>
2019-06-20 18:04:43 +0300

    Added mock

--- FreemanBook/SportsStore/SportsStore.Domain/Abstract/IProductRepository.cs
+++ FreemanBook/SportsStore/SportsStore.Domain/Abstract/IProductRepository.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using SportsStore.Domain.Entities;
 
 namespace SportsStore.Domain.Abstract
 {
-	interface IProductRepository
+	public interface IProductRepository
 	{
 		IQueryable<Product> Products { get; }
 
