7f6b09505fa1d5c2ef3d0f9ae41172680b62d054
vdobrzhanskyi <vdobrzhanskyi@skelia.com>
2019-06-25 16:49:42 +0300

    Old project added

---
+++ CashBox/CashBox/Item.cs
@@ -0,0 +1,16 @@
+﻿namespace CashBox
+{
+	class Item
+	{
+		public int Balance;
+		public int Barcode;
+		public int Price;
+
+		public Item(int barcode, int price,int balance)
+		{
+			this.Barcode = barcode;
+			this.Balance = balance;
+			this.Price = price;
+		}
+	}
+}
---
+++ CashBox/CashBox/Operations.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+namespace CashBox
+{
+	//public List<Stock> stock = new List<Stock>();
+	//public void createStock(int barcode,int count)
+	//{
+	//	stock.Add(new Stock(getItemFromBarcode(barcode), count));
+	//}
+	//private Item GetItemFromBarcode(int barcode)
+	//{
+	//	foreach (var items in itemList)
+	//	{
+	//		if (items.Barcode == barcode)
+	//		{
+	//			return items;
+	//		}
+	//	}
+	//	return null;
+	//}
+
+	class Operations
+	{
+		public List<Item> itemList = new List<Item>();
+
+		public void createItem(int barcode, int price,int balance)
+		{
+			itemList.Add(new Item(barcode, price, balance));
+		}
+	
+		public void SellFromItem(int barcode,int count)
+		{		
+			for (int i = 0; i < itemList.Count; i++)
+				{
+				Item item = itemList[i];
+				if (item.Barcode == barcode)
+				{
+					int tmpCount = item.Balance;
+					if (count >= tmpCount)
+					{
+						RemoveItem(item);
+						count -= tmpCount;
+						i--;
+					}
+					else
+					{
+						item.Balance -= count;
+						break;
+					}
+				}
+			}
+		}
+
+		private void RemoveItem(Item item)
+		{
+			itemList.Remove(item);
+		}
+	}
+}
