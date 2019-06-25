4ff33c548f965fec4f50396c19fefda4fec7fff9
vdobrzhanskyi <vdobrzhanskyi@skelia.com>
2019-06-25 15:39:47 +0300

    Added Asp.Net Course by metanit

---
+++ Metanit_ASP.NET MVC/FirstApp/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.Mvc;
+using FirstApp.Models;
+
+namespace FirstApp.Controllers
+{
+	public class HomeController : Controller
+	{
+		BookContext db = new BookContext();
+		public ActionResult Index()
+		{
+			var books = db.Books;
+			ViewBag.Books = books;
+			return View();
+		}
+		[HttpGet]
+		public ActionResult Buy(int id)
+		{
+			ViewBag.BookId = id;
+			return View();
+		}
+		[HttpPost]
+		public string Buy(Purchase purchase)
+		{
+			purchase.Date = DateTime.Now;
+			db.Purchases.Add(purchase);
+			db.SaveChanges();
+			return "Thank you, " + purchase.Person + ",for your order";
+		}
+	}
+}
---
+++ Metanit_ASP.NET MVC/FirstApp/Models/Purchase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FirstApp.Models
+{
+	public class Purchase
+	{
+		public int PurchaseId { get; set; }
+
+		public string Person { get; set; }
+
+		public string Adress { get; set; }
+
+		public int BookId { get; set; }
+
+		public DateTime Date { get; set; }
+	}
+}
