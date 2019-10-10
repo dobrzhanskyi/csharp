99dfe21335db4d6d4ef87abc000d252ddfa70139
vdobrzhanskyi <vdobrzhanskyi@skelia.com>
2019-10-10 16:54:59 +0300

    Created Controller for Menu with categories and added test for categories

--- FreemanBook/SportsStore/SportsStore.UnitTests/AllTests.cs
+++ FreemanBook/SportsStore/SportsStore.UnitTests/AllTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using SportsStore.Domain.Abstract;
@@ -18,11 +19,11 @@ namespace SportsStore.UnitTests
 			Mock<IProductRepository> mock = new Mock<IProductRepository>();
 			mock.Setup(m => m.Products).Returns(new Product[]
 			{
-			new Product { ProductID = 1, Name = "P1" },
-			new Product { ProductID = 2, Name = "P2" },
-			new Product { ProductID = 3, Name = "P3" },
-			new Product { ProductID = 4, Name = "P4" },
-			new Product { ProductID = 5, Name = "P5" }
+				new Product { ProductID = 1, Name = "P1" },
+				new Product { ProductID = 2, Name = "P2" },
+				new Product { ProductID = 3, Name = "P3" },
+				new Product { ProductID = 4, Name = "P4" },
+				new Product { ProductID = 5, Name = "P5" }
 			}.AsQueryable());
 
 			ProductController controller = new ProductController(mock.Object);
@@ -45,11 +46,11 @@ namespace SportsStore.UnitTests
 			Mock<IProductRepository> mock = new Mock<IProductRepository>();
 			mock.Setup(m => m.Products).Returns(new Product[]
 			{
-			new Product { ProductID = 1, Name = "P1", Category = "Cat1" },
-			new Product { ProductID = 2, Name = "P2", Category = "Cat2" },
-			new Product { ProductID = 3, Name = "P3", Category = "Cat1" },
-			new Product { ProductID = 4, Name = "P4", Category = "Cat2" },
-			new Product { ProductID = 5, Name = "P5", Category = "Cat3" }
+				new Product { ProductID = 1, Name = "P1", Category = "Cat1" },
+				new Product { ProductID = 2, Name = "P2", Category = "Cat2" },
+				new Product { ProductID = 3, Name = "P3", Category = "Cat1" },
+				new Product { ProductID = 4, Name = "P4", Category = "Cat2" },
+				new Product { ProductID = 5, Name = "P5", Category = "Cat3" }
 			}.AsQueryable());
 
 			ProductController controller = new ProductController(mock.Object);
@@ -62,6 +63,28 @@ namespace SportsStore.UnitTests
 			Assert.AreEqual(result.Length, 2);
 			Assert.IsTrue(result[0].Name == "P2" && result[0].Category == "Cat2");
 			Assert.IsTrue(result[1].Name == "P4" && result[1].Category == "Cat2");
+		}
+
+		[TestMethod]
+		public void Can_Create_Categories()
+		{
+			Mock<IProductRepository> mock = new Mock<IProductRepository>();
+			mock.Setup(m => m.Products).Returns(new Product[]
+			{
+				new Product { ProductID = 1, Name = "P1", Category = "Apples" },
+				new Product { ProductID = 2, Name = "P2", Category = "Apples" },
+				new Product { ProductID = 3, Name = "P3", Category = "Plums" },
+				new Product { ProductID = 4, Name = "P4", Category = "Oranges" },
+			}.AsQueryable());
+
+			NavController target = new NavController(mock.Object);
+
+			string[] results = ((IEnumerable<string>)target.Menu().Model).ToArray();
+
+			Assert.AreEqual(results.Length, 3);
+			Assert.AreEqual(results[0], "Apples");
+			Assert.AreEqual(results[1], "Oranges");
+			Assert.AreEqual(results[2], "Plums");
 		}
 	}
 }
---
+++ FreemanBook/SportsStore/SportsStore.WebUI/Controllers/NavController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+		// GET: Nav
+		private IProductRepository _repo;
+
+		public NavController(IProductRepository repo)
+		{
+			_repo = repo;
+		}
+
+		public PartialViewResult Menu()
+		{
+			IEnumerable<string> categories = _repo.Products
+				.Select(x => x.Category)
+				.Distinct()
+				.OrderBy(x => x);
+			return PartialView(categories);
+		}
+    }
+}
