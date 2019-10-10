2cbb3db12bdd2babacd48e9cf4c41d416d58a217
vdobrzhanskyi <vdobrzhanskyi@skelia.com>
2019-10-10 17:26:08 +0300

    Added indication of selected category. Number of pages depends now of product count in specific category

--- FreemanBook/SportsStore/SportsStore.UnitTests/AllTests.cs
+++ FreemanBook/SportsStore/SportsStore.UnitTests/AllTests.cs
@@ -86,5 +86,49 @@ namespace SportsStore.UnitTests
 			Assert.AreEqual(results[1], "Oranges");
 			Assert.AreEqual(results[2], "Plums");
 		}
+
+		[TestMethod]
+		public void Is_Selected_Category_Indicated()
+		{
+			Mock<IProductRepository> mock = new Mock<IProductRepository>();
+			mock.Setup(m => m.Products).Returns(new Product[]
+			{
+				new Product { ProductID = 1, Name = "P1", Category = "Apples" },
+				new Product { ProductID = 2, Name = "P2", Category = "Oranges" },
+			}.AsQueryable());
+
+			NavController target = new NavController(mock.Object);
+			string categoryToSelect = "Apples";
+			string result = target.Menu(categoryToSelect).ViewBag.SelectedCategory;
+
+			Assert.AreEqual(categoryToSelect, result);
+		}
+
+		[TestMethod]
+		public void Generate_Category_Specific_Product_Count()
+		{
+			Mock<IProductRepository> mock = new Mock<IProductRepository>();
+			mock.Setup(m => m.Products).Returns(new Product[]
+			{
+				new Product { ProductID = 1, Name = "P1", Category = "Cat1" },
+				new Product { ProductID = 2, Name = "P2", Category = "Cat2" },
+				new Product { ProductID = 3, Name = "P3", Category = "Cat1" },
+				new Product { ProductID = 4, Name = "P4", Category = "Cat2" },
+				new Product { ProductID = 5, Name = "P5", Category = "Cat3" }
+			}.AsQueryable());
+
+			ProductController target = new ProductController(mock.Object);
+			target.PageSize = 3;
+
+			int resFirst = ((ProductListViewModel)target.List("Cat1").Model).PagingInfo.TotalItems;
+			int resSecond = ((ProductListViewModel)target.List("Cat2").Model).PagingInfo.TotalItems;
+			int resThird = ((ProductListViewModel)target.List("Cat3").Model).PagingInfo.TotalItems;
+			int resAll = ((ProductListViewModel)target.List(null).Model).PagingInfo.TotalItems;
+
+			Assert.AreEqual(resFirst, 2);
+			Assert.AreEqual(resSecond, 2);
+			Assert.AreEqual(resThird, 1);
+			Assert.AreEqual(resAll, 5);
+		}
 	}
 }
--- FreemanBook/SportsStore/SportsStore.WebUI/Controllers/NavController.cs
+++ FreemanBook/SportsStore/SportsStore.WebUI/Controllers/NavController.cs
@@ -1,30 +1,28 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
 
 namespace SportsStore.WebUI.Controllers
 {
-    public class NavController : Controller
-    {
+	public class NavController : Controller
+	{
 		// GET: Nav
-		private IProductRepository _repo;
+		private IProductRepository _repository;
 
 		public NavController(IProductRepository repo)
 		{
-			_repo = repo;
+			_repository = repo;
 		}
 
-		public PartialViewResult Menu()
+		public PartialViewResult Menu(string category = null)
 		{
-			IEnumerable<string> categories = _repo.Products
+			ViewBag.SelectedCategory = category;
+			IEnumerable<string> categories = _repository.Products
 				.Select(x => x.Category)
 				.Distinct()
 				.OrderBy(x => x);
 			return PartialView(categories);
 		}
-    }
+	}
 }
