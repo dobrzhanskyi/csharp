9d9eade9930d51916c7df1ff49a9d9e4e530b302
vdobrzhanskyi <vdobrzhanskyi@skelia.com>
2019-10-24 18:18:35 +0300

    Added CartController with methods
    Created Entity  for Cart.
    Created method for Adding products to cart
    ProductSummaryView changed=>added Add to cart button
    Created Unit Test for Cart
    Created Cart View with Continue shopping button that handles ReturnUrl

---
+++ FreemanBook/SportsStore/SportsStore.WebUI/Controllers/CartController.cs
@@ -0,0 +1,60 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
+
+namespace SportsStore.WebUI.Controllers
+{
+	public class CartController : Controller
+	{
+		private IProductRepository _repository;
+
+		public CartController(IProductRepository repo)
+		{
+			_repository = repo;
+		}
+
+		public ViewResult Index(string returnUrl)
+		{
+			return View(new CartIndexViewModel
+			{
+				Cart = getCart(),
+				ReturnUrl = returnUrl
+			});
+		}
+
+		public RedirectToRouteResult AddToCart(int productId, string returnUrl)
+		{
+			Product product = _repository.Products
+				.FirstOrDefault(p => p.ProductID == productId);
+			if (product != null)
+			{
+				getCart().AddItem(product, 1);
+			}
+			return RedirectToAction("Index", new { returnUrl });
+		}
+
+		public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
+		{
+			Product product = _repository.Products
+				.FirstOrDefault(p => p.ProductID == productId);
+			if (product != null)
+			{
+				getCart().RemoveLine(product);
+			}
+			return RedirectToAction("Index", new { returnUrl });
+		}
+
+		private Cart getCart()
+		{
+			Cart cart = (Cart)Session["Cart"];
+			if (cart == null)
+			{
+				cart = new Cart();
+				Session["Cart"] = cart;
+			}
+			return cart;
+		}
+	}
+}
